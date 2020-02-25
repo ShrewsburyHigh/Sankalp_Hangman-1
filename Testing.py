833629130cdf698682267ec0a58d83cc698780a1
mkgood20 <60886177+mkgood20@users.noreply.github.com>
2020-02-25 09:18:54 -0500

    Update Testing.py
    update

--- Testing.py
+++ Testing.py
@@ -1,8 +1,30 @@
-
+import random
+randomWords = ["ducks" , "jumbo" ,"lucky" , "pills" , "flour"]
+secret = random.choice (randomWords)
+letter = ""
+updateWord = []
+#print secret 
+def initialize(): 
+    print "We have a secret word"
+    print "_ _ _ _ _"
+def getLetter():
+    print "Enter a letter"
+    global letter
+    letter = raw_input()     
+def ifWon():
+    if secret == updateWord: 
+        print "you win"
+    else:
+        getLetter()
 def test(Letter):
     global updatedWord
-    global Letter
+    global letter
     global Secret
     if Letter in Secret:
         updatedWord.append(Letter)
         ifwon()
+def main():
+    initialize ()
+    getLetter()
+    ifWon()
+main()
